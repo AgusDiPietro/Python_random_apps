b606fbe6dd0b8fa703f7b2ca586637716597a312
AgusDiPietro <agusdipietro@live.com.ar>
2023-11-02 20:51:26 +0100

    some changes

--- Triangle_Area_Calculation.py
+++ Triangle_Area_Calculation.py
@@ -3,6 +3,9 @@ import math
 side_a = float(input("What is the first leg of the triangle?:"));
 side_b = float(input("What is the second leg of the triangle?:"));
 
+side_a = round(side_a, 1);
+side_b = round(side_b, 1);
+
 #Calculation
 side_c = math.sqrt(side_a**2 + side_b**2);
 side_c = round(side_c, 3);
@@ -11,5 +14,5 @@ area = 0.5*side_a*side_b;
 area = round(area, 3);
 
 #Summary
-print("\nFor a triangle with legs of " + str(side_a) + "and" + str(side_b) + "the hypotenyse is q1" + str(side_c) + ".");
+print("\nFor a triangle with legs of " + str(side_a) + " and " + str(side_b) + " the hypotenyse is " + str(side_c) + ".");
 print("\nAnd the area is " + str(area) + ".");
